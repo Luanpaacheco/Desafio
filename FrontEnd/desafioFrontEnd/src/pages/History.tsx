a1f0c9fb93d7cc5647a66480b4af2b5c9ea167e0
Luanpaacheco <luan.005@edu.pucrs.br>
2025-05-30 02:40:15 -0300

    feat: create history page

---
+++ FrontEnd/desafioFrontEnd/src/pages/History.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { MessegeChat } from "../components/MessageChat";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
+
+interface Message {
+  id: string;
+  text: string;
+  isUser: boolean;
+  createdAt: string;
+  userId: string;
+}
+
+const History = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/chat", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        const data = await res.json();
+        setMessages(data.reverse()); // <- inverte a ordem aqui
+      } catch (err) {
+        console.error("Erro ao buscar histórico:", err);
+      }
+    };
+
+    fetchHistory();
+  }, []);
+
+  return (
+    <div className="flex flex-col h-screen w-full px-4 py-2">
+      <div className="h-[8vh] flex items-center">
+        <button
+          className="flex gap-2 items-center text-sm underline cursor-pointer"
+          onClick={() => navigate("/chat")}
+        >
+          <ArrowBackIcon />
+          Voltar
+        </button>
+      </div>
+
+      <div className="flex-1 overflow-y-auto flex flex-col gap-3 mt-4 scrollbar-hide">
+        {messages.map((msg) => (
+          <MessegeChat key={msg.id} text={msg.text} isUser={msg.isUser} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default History;
